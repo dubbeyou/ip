e059abf3f860a8c847980d4c93fc9377138c88e2
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-19 14:39:07 +0800

    Add frameMessage function

--- src/main/java/Snek.java
+++ src/main/java/Snek.java
@@ -5,17 +5,21 @@ public class Snek {
     private static final String HELLO = "Ssss... Hello I'm Snek! Ssss...\nWhatsss cans I do for you todayss...?";
     private static final String BYE = "Ssss... Bye! Ssss...";
 
+    private static String frameMessage(String input) {
+        return LINEBREAK + "\n" + input + "\n" + LINEBREAK;
+    }
+
     public static void main(String[] args) {
-        System.out.println(LINEBREAK + "\n" + HELLO + "\n" + LINEBREAK);
+        System.out.println(frameMessage(HELLO));
 
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(input);
+            System.out.println(frameMessage(input));
             input = sc.nextLine();
         }
 
-        System.out.println(BYE + "\n" + LINEBREAK);
+        System.out.println(frameMessage(BYE));
         sc.close();
     }
 }
