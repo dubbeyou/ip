2d0812f3be242926fd645491cafa29f1de26fca0
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-19 14:30:46 +0800

    Add echo logic

--- src/main/java/Snek.java
+++ src/main/java/Snek.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Snek {
     private static final String LINEBREAK = "------------------------------------------------------------";
     private static final String HELLO = "Ssss... Hello I'm Snek! Ssss...\nWhatsss cans I do for you todayss...?";
@@ -5,6 +7,15 @@ public class Snek {
 
     public static void main(String[] args) {
         System.out.println(LINEBREAK + "\n" + HELLO + "\n" + LINEBREAK);
+
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = sc.nextLine();
+        }
+
         System.out.println(BYE + "\n" + LINEBREAK);
+        sc.close();
     }
 }
