2e42ad454a7988a6e9de8c662fe883040caf3154
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-19 15:06:18 +0800

    Add to-do list logic

--- src/main/java/Snek.java
+++ src/main/java/Snek.java
@@ -5,8 +5,27 @@ public class Snek {
     private static final String HELLO = "Ssss... Hello I'm Snek! Ssss...\nWhatsss cans I do for you todayss...?";
     private static final String BYE = "Ssss... Bye! Ssss...";
 
+    private static String[] toDoList = new String[100];
+    private static int toDoCount = 0;
+
     private static String frameMessage(String input) {
         return LINEBREAK + "\n" + input + "\n" + LINEBREAK;
+    }
+
+    private static void addToDo(String task) {
+        toDoList[toDoCount] = task;
+        toDoCount++;
+    }
+
+    private static void printToDoList() {
+        String res = "";
+        for (int i = 0; i < toDoCount; i++) {
+            res += (i + 1) + ". " + toDoList[i];
+            if (i != toDoCount - 1) {
+                res += "\n";
+            }
+        }
+        System.out.println(frameMessage(res));
     }
 
     public static void main(String[] args) {
@@ -15,7 +34,12 @@ public class Snek {
         Scanner sc = new Scanner(System.in);
         String input = sc.nextLine();
         while (!input.equals("bye")) {
-            System.out.println(frameMessage(input));
+            if (input.equals("list")) {
+                printToDoList();
+            } else {
+                System.out.println(frameMessage("I'ves addedss: " + input));
+                addToDo(input);
+            }
             input = sc.nextLine();
         }
 
