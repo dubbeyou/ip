4e4315501994654faf4b436023dc39acba3234af
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-19 18:08:24 +0800

    Add InvalidCommandSnekException.java

---
+++ src/main/java/InvalidCommandSnekException.java
@@ -0,0 +1,7 @@
+public class InvalidCommandSnekException extends SnekException {
+
+    public InvalidCommandSnekException(String message) {
+        super("Ssssory, I don't undersssstand the commandsss...");
+    }
+
+}
