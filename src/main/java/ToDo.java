5e29ea1994a635d26c00ce056e115453698620f1
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-19 20:25:26 +0800

    Integrate TaskType to task classes

--- src/main/java/ToDo.java
+++ src/main/java/ToDo.java
@@ -1,11 +1,11 @@
 public class ToDo extends Task {
 
     public ToDo(String description) {
-        super(description);
+        super(description, TaskType.TODO);
     }
 
     @Override
     public String toString() {
-        return "[T]" + super.toString();
+        return super.toString();
     }
 }
