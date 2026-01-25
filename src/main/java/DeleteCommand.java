9146920626db3de7bb61c49feccd7fa98278d6f4
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-25 15:46:16 +0800

    Add DeleteCommand.java

---
+++ src/main/java/DeleteCommand.java
@@ -0,0 +1,20 @@
+public class DeleteCommand extends Command {
+    private final String taskNumber;
+
+    public DeleteCommand(String taskNumber) {
+        this.taskNumber = taskNumber;
+    }
+
+    @Override
+    public void execute(TaskList tasks, Ui ui, Storage storage) throws SnekException {
+        int index;
+        try {
+            index = Integer.valueOf(taskNumber);
+        } catch (NumberFormatException e) {
+            throw new InvalidArgumentSnekException(Messages.MESSAGE_INVALID_TASK);
+        }
+        Task removedTask = tasks.delete(index);
+        ui.print(String.format(Messages.MESSAGE_DELETE_TASK, removedTask, tasks.size()));
+        storage.overwrite(tasks.getTasks());
+    }
+}
