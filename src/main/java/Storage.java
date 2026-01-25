31e94a6e10d761fbf73abb48bf29e654f4e0a0be
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-25 14:31:33 +0800

    Update Storage.java

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -5,8 +5,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-import javax.management.openmbean.ArrayType;
-
 public class Storage {
     private static final String STORAGEPATH = "./data";
     private static final String FILENAME = "snek.txt";  
@@ -15,9 +13,10 @@ public class Storage {
 
     public Storage() {
         this.file = new File(STORAGEPATH, FILENAME);
+        createFile();
     }
 
-    private void createStorageFile() {
+    private void createFile() {
         File dir = new File(STORAGEPATH);
         if (!dir.exists()) {
             dir.mkdirs();
@@ -29,17 +28,37 @@ public class Storage {
         }
     }
 
-    public ArrayList<Task> loadTasks() {
+    public ArrayList<Task> loadTasks() throws SnekException{
         ArrayList<Task> taskList = new ArrayList<>();
         try (Scanner scanner = new Scanner(file)) {
             while (scanner.hasNextLine()) {
                 String line = scanner.nextLine();
-                Task task = parser.parseTaskFromFile(line); // TO BE IMPLEMENTED
+                Task task = Parser.parseTaskFromFile(line);
                 taskList.add(task);
             }
         } catch (FileNotFoundException ignore) {
-            return new ArrayList<>()
+            return new ArrayList<>();
         }
         return taskList;
+    }
+
+    public void write(Task task) {
+        try (FileWriter fw = new FileWriter(file, true)) {
+            fw.write(task.getSaveString() + "\n");
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to write to storage file.", e);
+        }
+    }
+
+    public void overwrite(ArrayList<Task> taskList) {
+        try (FileWriter fw = new FileWriter(file, false)) {
+            for (Task task : taskList) {
+                fw.write(task.getSaveString() + "\n");
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to overwrite storage file.", e);
+        }
     }
 }
