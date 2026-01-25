151c0d9e66b42dbf39d6f1976684ef978682f9f8
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-25 14:31:21 +0800

    Update Ui.java

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,8 @@
+import java.util.Scanner;
+
 public class Ui {
+    private final Scanner scanner = new Scanner(System.in);
+
     public void print(String message) {
         System.out.println(Messages.MESSAGE_LINEBREAK + "\n" + message + "\n" + Messages.MESSAGE_LINEBREAK);
     }
@@ -13,5 +17,9 @@ public class Ui {
 
     public void printByeMessage() {
         print(Messages.MESSAGE_BYE);
+    }
+
+    public String getUserInput() {
+        return scanner.nextLine();
     }
 }
