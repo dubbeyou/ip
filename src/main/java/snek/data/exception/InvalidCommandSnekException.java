3ae606b2a1a669c56f203396982925a1f9adb40c
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-26 12:51:31 +0800

    Add JavaDoc comments for exceptions

--- src/main/java/snek/data/exception/InvalidCommandSnekException.java
+++ src/main/java/snek/data/exception/InvalidCommandSnekException.java
@@ -2,6 +2,9 @@ package snek.data.exception;
 
 import static snek.common.Messages.MESSAGE_INVALID_COMMAND;
 
+/**
+ * Exception thrown for invalid commands in the Snek application.
+ */
 public class InvalidCommandSnekException extends SnekException {
     public InvalidCommandSnekException() {
         super(MESSAGE_INVALID_COMMAND);
--- src/main/java/snek/data/exception/SnekException.java
+++ src/main/java/snek/data/exception/SnekException.java
@@ -1,6 +1,16 @@
 package snek.data.exception;
 
+/**
+ * Base exception class for Snek application.
+ * Exception thrown for general errors in the Snek application.
+ */
 public class SnekException extends Exception {
+
+    /**
+     * Constructs a SnekException with the specified error message.
+     *
+     * @param message The error message.
+     */
     public SnekException(String message) {
         super(message);
     }
