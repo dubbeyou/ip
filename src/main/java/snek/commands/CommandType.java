d90d16cadb87cb314debe2a34eb326bb8a35f87e
dubbeyou <86919346+dubbeyou@users.noreply.github.com>
2026-01-26 13:55:00 +0800

    CommandType.java: Add FIND command

--- src/main/java/snek/commands/CommandType.java
+++ src/main/java/snek/commands/CommandType.java
@@ -3,13 +3,13 @@ package snek.commands;
 import snek.data.exception.InvalidCommandSnekException;
 
 public enum CommandType {
-    LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, BYE;
+    LIST, MARK, UNMARK, TODO, DEADLINE, EVENT, DELETE, FIND, BYE;
 
     public static CommandType from(String token) throws InvalidCommandSnekException {
         if (token == null || token.trim().isEmpty()) {
             throw new InvalidCommandSnekException();
         }
-        
+
         try {
             return CommandType.valueOf(token.toUpperCase());
         } catch (IllegalArgumentException e) {
